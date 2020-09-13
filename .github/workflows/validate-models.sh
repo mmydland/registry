8b09b1e3648b8c9202e3d621b22e455404541042
ridomin <ridomin@live.com>
2020-09-13 00:31:33 -0700

    bash msgs

--- .github/workflows/validate-models.sh
+++ .github/workflows/validate-models.sh
@@ -3,9 +3,13 @@ do
     if [[ $f == *"dtmi/"* ]]
     then
         dtdl2-validator /f=$f /resolver=local
+        echo $?
         if [ $? -neq 0 ]
         then
+            echo "error validating model"
             exit 1
+        else
+            echo "validation ok"
         fi
     fi
 done
